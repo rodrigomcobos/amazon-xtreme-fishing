d80d11d0783c26c062c323fce8f62463db2ec17b
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-11-11 13:59:55 -0600

    created form components, redirects file for netlify compability, robots.txt file for SEO setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-3xl font-bold underline text-red-500'>Testing Tailwind</h1>
+    </div>
   )
 }
 
