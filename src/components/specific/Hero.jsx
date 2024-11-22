c943be2bcd5f1fb0faa8421bc1d2c60e9653efce
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-11-21 23:04:58 -0600

    made hero section responsive

--- src/components/specific/Hero.jsx
+++ src/components/specific/Hero.jsx
@@ -27,25 +27,25 @@ const Hero = () => {
             <div className="absolute inset-0 bg-gradient-to-t from-transparent to-black/80 h-32" />
 
             {/* Content Container */}
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-white z-10">
-                <p className="text-base mb-2 max-w-2xl text-center font-dmsans uppercase">
+            <section className="absolute inset-0 flex flex-col items-center justify-center text-white z-10 px-2 md:px-6">
+                <p className="text-sm sm:text-base mb-2 max-w-2xl text-center font-dmsans uppercase">
                     Catch the Ultimate Trophy
                 </p>
-                <h1 className="text-5xl font-roxale uppercase mb-1">
+                <h1 className="text-2xl sm:text-4xl md:text-5xl font-roxale uppercase mb-1 text-center">
                     Peacock Bass Fishing in the
                 </h1>
-                <h1 className="text-7xl font-roxale font-bold uppercase mb-10">
+                <h1 className="text-3xl sm:text-5xl md:text-7xl font-roxale font-bold uppercase mb-6 sm:mb-10 text-center">
                     Heart of the Amazon
                 </h1>
-                <section className="flex flex-col sm:flex-row gap-5">
-                    <button className="ring-1 ring-white text-sm hover:bg-secondary hover:ring-1 hover:ring-fifth hover:text-fifth text-white font-normal py-3 px-8 rounded-md transition duration-500 uppercase font-dmsans">
+                <section className="flex flex-col sm:flex-row gap-4 sm:gap-5 w-full max-w-lg sm:max-w-2xl justify-center">
+                    <button className="w-full sm:w-auto ring-1 ring-white text-sm hover:bg-secondary hover:ring-1 hover:ring-fifth hover:text-fifth text-white font-normal py-3 px-8 rounded-md transition duration-500 uppercase font-dmsans">
                         Plan Your Adventure
                     </button>
-                    <button className="ring-1 ring-white text-sm hover:bg-secondary hover:ring-1 hover:ring-fifth hover:text-fifth text-white font-normal py-3 px-8 rounded-md transition duration-500 uppercase font-dmsans">
+                    <button className="w-full sm:w-auto ring-1 ring-white text-sm hover:bg-secondary hover:ring-1 hover:ring-fifth hover:text-fifth text-white font-normal py-3 px-8 rounded-md transition duration-500 uppercase font-dmsans">
                         Learn More About Peacock Bass
                     </button>
                 </section>
-            </div>
+            </section>
         </section>
     );
 };
