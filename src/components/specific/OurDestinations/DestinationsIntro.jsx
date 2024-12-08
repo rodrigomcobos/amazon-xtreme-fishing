3960c78a20f3c56bae27cd7c5d5bdccf5e811c22
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-12-08 17:29:56 -0600

    adjusted the content max width

--- src/components/specific/OurDestinations/DestinationsIntro.jsx
+++ src/components/specific/OurDestinations/DestinationsIntro.jsx
@@ -4,10 +4,10 @@ import HeroImg from '../../../assets/images/destinationsheroimg.png';
 const DestinationsIntro = () => {
     return (
         <div className="bg-tertiary px-4 py-12 mt-32">
-            <div className="grid lg:grid-cols-2 gap-12 lg:max-w-6xl max-w-9xl mx-auto">
+            <div className="grid lg:grid-cols-2 gap-12 lg:max-w-7xl max-w-7xl mx-auto">
                 <div className="text-left">
                     <p className='text-base text-white uppercase font-dmsans font-light mb-2'>World Class Accommodations</p>
-                    <h2 className="text-white text-3xl sm:text-5xl font-roxale italic font-bold mb-6">Have you planned your fishing trip yet <span className='font-cormorant italic'>?</span></h2>
+                    <h2 className="text-white text-3xl sm:text-5xl font-roxale italic font-bold mb-6">Have you planned your fishing trip yet <span className='font-cormorant italic font-light'>?</span></h2>
                     <p className="mb-4 text-sm text-white font-dmsans font-light">Whether you're after a premier luxury lodge, the #1 live-aboard houseboat, a private yacht, remote river expeditions, or exploratory fishing adventures, we'll help you find the perfect destination tailored to your travel needs, schedule, fishing goals, and budget. With years of hands-on experience and intimate knowledge of our regions, we provide expert guidance to create unforgettable experiences. If it's not already in our offerings, we'll customize it for you—whether it's outdoor TV shows, documentaries, research expeditions, missionary trips, or educational excursions. Even if you just want to escape on a private boat for a week, we'll turn your vision into reality.</p>
                 </div>
                 <div className="-mt-24">
