079bd4709b316935af0112cd2c55ba46068d44ef
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-12-07 21:13:46 -0600

    added a new component for the intro, setup structure, just need to work on getting the styling to work

--- src/components/specific/OurDestinations/Hero.jsx
+++ src/components/specific/OurDestinations/Hero.jsx
@@ -18,11 +18,12 @@ const Hero = () => {
 
             {/* Content Container */}
             <section className="absolute inset-0 flex flex-col items-center justify-center text-white z-10 px-2 md:px-6 pt-24 sm:pt-24">
-                <p className="text-sm sm:text-base mb-2 max-w-2xl text-center font-dmsans uppercase">
+                <p className="text-sm sm:text-base mb-1 max-w-2xl text-center font-dmsans uppercase">
                     All Inclusive
                 </p>
+                <hr className='w-[10%] border-t-2 border-fifth mb-2' />
                 <h1 className="text-2xl sm:text-4xl md:text-5xl font-roxale-calligraphy font-bold mb-1 text-center">
-                    The Ultimate Peacock Bass
+                    Ultimate Peacock Bass
                 </h1>
                 <h1 className="text-3xl sm:text-5xl md:text-6xl font-roxale font-bold uppercase mb-2 sm:mb-0 text-center ">
                     Fishing Destinations
--- src/pages/Destinations/AllDestinations.jsx
+++ src/pages/Destinations/AllDestinations.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/specific/OurDestinations/Hero'
 import DestinationsStrip from '../../components/specific/OurDestinations/DestinationsStrip'
+import DestinationsIntro from '../../components/specific/OurDestinations/DestinationsIntro'
 
 const AllDestinations = () => {
     return (
         <div>
             <Hero />
             <DestinationsStrip />
+            <DestinationsIntro />
         </div>
     )
 }
