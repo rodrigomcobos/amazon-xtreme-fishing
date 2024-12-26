f5ccf8e230d737ef2b80ba3521e60cce194acca1
Rodrigo Cobos <101303234+rodrigomcobos@users.noreply.github.com>
2024-12-25 21:17:59 -0600

    adjusted responsiveness on whichpackageisright section, it was out of wack on table breakpoint

--- src/components/specific/OurDestinations/DestinationsStructure/SpotlightVideo.jsx
+++ src/components/specific/OurDestinations/DestinationsStructure/SpotlightVideo.jsx
@@ -4,7 +4,7 @@ import Topobackground2 from '../../../../assets/images/topobackground2.svg';
 
 const SpotlightVideo = () => {
     return (
-        <div className="bg-tertiary px-4 pt-14 sm:pt-10 border-t-4 border-fifth relative pb-10 sm:pb-0">
+        <div className="bg-tertiary pt-14 sm:pt-10 border-t-4 border-fifth relative pb-10 sm:pb-0">
             {/* Wrapper div for background with overflow hidden */}
             <div className="absolute inset-0 overflow-hidden">
                 <img
@@ -14,10 +14,10 @@ const SpotlightVideo = () => {
                 />
             </div>
 
-            <div className="grid lg:grid-cols-2 gap-0 lg:gap-24 lg:max-w-9xl max-w-7xl mx-auto">
-                {/* Video Section - Column 1*/}
-                <section className="order-1 relative bottom-24">
-                    <div className="relative overflow-hidden rounded-lg h-[20rem] sm:h-[30rem] w-[25rem] sm:w-[48rem]">
+            <div className="flex flex-col xl:grid xl:grid-cols-2 gap-0 xl:gap-14 xl:max-w-9xl max-w-7xl mx-auto">
+                {/* Video Section */}
+                <section className="relative bottom-24 mx-4 sm:mx-0 right-0 sm:right-4">
+                    <div className="relative overflow-hidden rounded-lg h-[20rem] sm:h-[30rem] w-full sm:w-[50rem] mx-auto xl:mx-0">
                         <video
                             className="rounded-lg object-cover w-full h-full"
                             autoPlay
@@ -31,15 +31,15 @@ const SpotlightVideo = () => {
                     </div>
                 </section>
 
-                {/* Text Content  - Column 2*/}
-                <section className="text-left order-2 ml-0 sm:ml-32 z-10 -mt-14 sm:mt-4">
+                {/* Text Content */}
+                <section className="text-center lg:text-center xl:text-left mx-4 sm:mx-auto xl:mx-0 xl:ml-44 max-w-4xl xl:max-w-none z-10 -mt-12 xl:mt-4">
                     <p className="text-sm sm:text-base text-white uppercase font-dmsans font-light mb-4">
                         The River's at your mercy
                     </p>
-                    <h2 className="text-white text-2xl sm:text-4xl font-roxale italic font-bold mb-6">
+                    <h2 className="text-white text-3xl sm:text-4xl font-roxale italic font-bold mb-6">
                         The World<span className="text-white font-cormorant italic"> '</span>s Most Demanding Anglers Fish with Us
                     </h2>
-                    <p className="mb-4 text-sm text-white font-dmsans font-light">
+                    <p className="mb-4 lg:mb-10 text-sm text-white font-dmsans font-light">
                         Born from a deep passion for the Amazon Forest and a commitment to excellence in sport fishing, The Zaltan Floating Hotel redefines world-class fishing in the Upper Rio Negro region. Designed to meet the highest expectations, it delivers an unparalleled Peacock Bass fishing experience in the heart of a new frontier. With its indigenous-inspired name, The Zaltan stands as a landmark in sport fishing, setting a new standard not only in the Amazon but across all of Brazil.
                     </p>
                 </section>
